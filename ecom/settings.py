70f0c2c004dd1b445bdce100aa073a6cc3384a23
Anbu Anandhurai <anandhurai2004@gmail.com>
2026-02-17 16:21:46 +0530

    Configured PostgreSQL database settings for Render deployment

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,5 +1,5 @@
 
-
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -88,13 +88,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3', 
-    }
-}
+# REPLACD YOUR OLD DATABASES SECTION WITH THIS DUE TO RENDER PLOTFORM DOESN'T SUPORT MORE THAN ONE POSTGRESQL DATABASE 
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
