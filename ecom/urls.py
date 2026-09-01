0a359e4dc8ea2a427a58b2dc93348ace40dd8c76
Anbu Anandhurai <anandhurai2004@gmail.com>
2026-09-01 15:25:20 +0530

    fix: correct oversized heading on 404 page by scoping styles and using inline theme overrides

--- ecom/urls.py
+++ ecom/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
+handler404 = 'ecom.views.custom_404' 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
